eeead5633dc80054eb1a956ef6050aa43ec2f66a
NomanRasheed1 <104938606+NomanRasheed1@users.noreply.github.com>
2022-05-05 13:05:31 +0200

    Create chp8-drill.cpp

---
+++ chp8-drill.cpp
@@ -0,0 +1,46 @@
+#include "std_lib_facilities.h"
+
+void swap_v(int a, int b) {
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+
+}
+
+void swap_r(int &a, int &b)
+{
+	int t;
+	t = a;
+	a = b;
+	b = t;
+}
+
+void swap_cr(const int& a, const int& b)
+{
+	int t = a;
+	a = b;
+	b = t;
+
+}
+int main()
+{
+	int x = 7;
+	int y = 9;
+	swap_v(x, y);
+	cout << x << "  " << y << endl;
+	swap_r(x, y);
+	cout << x << "  " << y << endl;
+	const int cx = 7;
+	const int cy = 9;
+
+	swap_cr(cx, cy);
+	cout << cx << "  " << cy << endl;
+	swap_v(7.7, 9.9);
+	
+	double dx = 7.7;
+	double dy = 9.9;
+	swap_r(dx, dy);
+	cout << dx << "  " << dy << endl;
+	return 0;
+}
